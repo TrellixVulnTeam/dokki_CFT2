52be688d8ee896adb4fc7123ee995af38f73db35
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-16 14:51:40 +0000

    Adding tarfile member sanitization to extractall()

--- dokki/utils.py
+++ dokki/utils.py
@@ -23,7 +23,26 @@ label_color_map = {k: distinct_colors[i] for i, k in enumerate(label_map.keys())
 
 def extract_tar(filename: str, output : str) -> None:
     with tarfile.open(filename, "r") as tar:
-        tar.extractall(path=output)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, path=output)
 
 def parse_annotation(annotation_path):
     tree = ET.parse(annotation_path)
